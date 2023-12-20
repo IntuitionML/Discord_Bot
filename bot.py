fcfe8fbdcbe9905c3c429114be43d1a28788278d
Josh <michaelsjoshua@gmail.com>
2023-12-20 00:18:18 -0500

    Minor Changes

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ Bot Current:
 - View channel for post with instagram.com/p/ or /reel/ and download/post the movie from the link
 
 ToDo:
+- Logging via local
 - Error Handling 
 - In-Discord functionality:
     - Start, Stop, Pause the bot
@@ -15,7 +16,7 @@ from discord import Interaction
 from discord.ext import commands
 from discord import app_commands
 
-
+import logging
 
 import os
 from dotenv import load_dotenv, find_dotenv
@@ -31,21 +32,20 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="/", intents=intents, heartbeat_timeout=0, description="A silly little bot that can do some silly little things.\n\n Use the prefix '/' with a command below.")
 
-
+# Setup logging
+logging_handler = logging.FileHandler(filename='events.log', encoding='utf-8', mode='w')
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user}')
+    logging.info(f"Logged in as {bot.user}")
+    await bot.change_presence(status=discord.Status.online, activity=discord.CustomActivity(name='🌽Online🌽'))
 
 @bot.hybrid_command(name="ping")
 async def ping(ctx):
     """ - Perform a health check"""
-    with open('allowed_channels.yaml', 'r') as file:
-        data = yaml.load(file, Loader=yaml.FullLoader)
-    allowed_channels = data['allowed_channels']
-    if ctx.channel.id not in allowed_channels:
-        return
-    await ctx.send(f"Bot online.\nChannel ID: {ctx.channel.id}")
+    if bot_can_see(ctx):
+        await ctx.send(f"Bot online.\nLatency: {bot.latency * 1000:.2f} ms")
 
 
 @bot.hybrid_command(name="allow")
@@ -59,9 +59,9 @@ async def allow(ctx):
         data['allowed_channels'].append(ctx.channel.id)
         with open('allowed_channels.yaml', 'w') as file:
             yaml.dump(data, file)
-        await ctx.send(f'Channel {ctx.channel.id} allowed.')
+        await ctx.send(f'Channel Allowed!')
     else:
-        await ctx.send(f'Channel {ctx.channel.id} already allowed.')
+        await ctx.send(f'Channel is already allowed.')
 
 @bot.hybrid_command()
 @commands.guild_only()
@@ -74,20 +74,50 @@ async def deny(ctx):
         data['allowed_channels'].remove(ctx.channel.id)
         with open('allowed_channels.yaml', 'w') as file:
             yaml.dump(data, file)
-        await ctx.send(f'Channel {ctx.channel.id} denied.')
+        await ctx.send(f'Channel denied!')
     else:
-        await ctx.send(f'Channel {ctx.channel.id} not in allowed list.')
+        await ctx.send(f'Channel is not in allowed list.')
+
+@bot.hybrid_command()
+@commands.guild_only()
+@commands.is_owner()
+async def test(ctx):
+    """ - Test Case"""
+    
+    if bot_can_see(ctx):
+        await ctx.send(f'Test successful')
+    else:
+        return
 
 
-# Loop and load cogs.
+def bot_can_see(ctx):
+    """
+    Determines if the bot has access to the channel that a message or command was posted in.
+    
+    Parameters:
+    - ctx (discord message): The discord message the bot is trying to respond to.
+    
+    Returns:
+    - bool"""
+    
+    with open('allowed_channels.yaml', 'r') as file:
+        data = yaml.load(file, Loader=yaml.FullLoader)
+    allowed_channels = data['allowed_channels']
+    if ctx.channel.id not in allowed_channels:
+        return False
+    return True
+
+
+# Load cogs.
 @bot.event
 async def setup_hook():
   for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         await bot.load_extension(f'cogs.{filename[:-3]}')
         print(f"Loaded Cog: {filename[:-3]}")
+        logging.info(f"Loaded Cog: {filename[:-3]}")
     else:
-        print("Unable to load pycache folder.")
+        return
 
 
 # Sync Command Tree. Needed for bot.hybrid_commands (Slash Commands)
@@ -98,6 +128,7 @@ async def sync(ctx):
     synced = await ctx.bot.tree.sync()
     await ctx.send (f"Synced {len(synced)} commands to current guild")
     print(f"Synced: {synced}")
+    logging.info(f"Synced: {synced}")
     return
 
-bot.run(DISCORD_TOKEN)
+bot.run(DISCORD_TOKEN, log_handler=logging_handler, log_level=logging.DEBUG)
--- cogs/ig_download.py
+++ cogs/ig_download.py
@@ -3,9 +3,11 @@ from discord.ext import commands
 
 import instaloader
 from instaloader import Post
+from instaloader.exceptions import QueryReturnedBadRequestException
 import os
 import re
 
+import logging
 import asyncio
 import yaml
     
@@ -25,7 +27,18 @@ class IGDownloader(commands.Cog):
         print("Checking URL")
         if ig_url_match:
             ig_url = ig_url_match.group(1)
-            await self.download_and_send_video(message, ig_url)
+            
+            try:
+                await self.download_and_send_video(message, ig_url)
+            
+            except QueryReturnedBadRequestException as e:
+                if e.response.status_code == 401:
+                    await message.reply("Error: JSON Query to graphql/query: HTTP error code 401..")
+            except Exception as e:
+                await message.reply(f"An unexpected error occurred: {e}")
+
+
+            # put error handling here?
     
     async def download_and_send_video(self, message, ig_url):
         # init reply
@@ -34,16 +47,17 @@ class IGDownloader(commands.Cog):
                 
         # Get shortcode
         shortcode = self.get_shortcode(ig_url)
-        print(f"Downloading shortcode: {shortcode}")
+        
         # Get Instaloader instance
         L = instaloader.Instaloader()
         # Only download video
-        L.download_pictures = False
+        L.download_pictures = True
         L.download_videos = True
         L.download_video_thumbnails = False
         L.download_geotags = False
         L.download_comments = False
         L.save_metadata = False
+    
         # Special requierment to avoid downloading caption as .txt
         L.post_metadata_txt_pattern = ""
         # Specify filename pattern
@@ -51,19 +65,36 @@ class IGDownloader(commands.Cog):
 
         # Get Post object from URL shortcode
         post = Post.from_shortcode(L.context, shortcode)
-
+        
         # Download content of Post object to temp folder
         L.download_post(post, target="temp")
-        video_path = f'temp/{shortcode}.mp4'
-        with open(video_path, 'rb') as video_file:
-            await asyncio.gather(
-                processing_message.delete(), 
-                message.reply(file=discord.File(video_file))
-            )
-        os.remove(video_path)                                 # delete IG video after poasted,
+        target_path = 'temp'
 
+        # Download post videos
+        file_payload = []       
+        for filename in os.listdir(target_path):
+            if filename.startswith(f'{shortcode}'):
+                file_path = os.path.join(target_path, filename)
+                file_payload.append(discord.File(file_path))
+        print(file_payload)
+        # Verify files in payload
+        if file_payload:
+            await asyncio.gather(
+                processing_message.delete(),
+                message.reply(files=file_payload, mention_author=False)
+            )
+        elif not file_payload:
+            logging.error(f"No files in found in .\{file_path}\nResult: {file_payload}")
+            await asyncio.gather(
+                message.reply(f"Bot encountered an error"),
+                processing_message.delete()
+            )
         
-    # Convert all ig url to "shortcode"
+        # Clean up temp folder Consider a try: except block?
+        for file in file_payload:
+            os.remove(file.fp.name)
+
+    # Convert ig url to "shortcode"
     def get_shortcode(self, ig_url):
         match = re.search(r'/p/([^/]+)|/reel/([^/?]+)', ig_url)
         if match:
